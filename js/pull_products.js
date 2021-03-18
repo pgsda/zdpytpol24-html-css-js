e6bcc4294ae10d0b46ed75af142114b016795351
Your Name <you@example.com>
2021-03-18 18:43:21 +0100

    Strona produktow

--- js/pull_products.js
+++ js/pull_products.js
@@ -1,8 +1,19 @@
-var xhttp = new XMLHttpRequest();
+function pull_products() {
+    var xhttp = new XMLHttpRequest();
 
-xhttp.onload = function () {
-    document.querySelector('main section.right').innerHTML = this.responseText;
+    xhttp.onload = function () {
+        var p = JSON.parse(this.responseText);
+
+        document.getElementById('products').innerHTML = '';
+
+        for (var i = 0; i < p.length; i++) {
+            document.getElementById('products').innerHTML +=
+                '<tr><td>' + p[i]['name'] + '</td></tr>';
+        }
+    }
+
+    xhttp.open("GET", "produkty.json", true);
+    xhttp.send();
 }
 
-xhttp.open("GET", "produkty.txt", true);
-xhttp.send();
+document.getElementsByName('load')[0].addEventListener('click', pull_products);
