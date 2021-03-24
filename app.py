8328451dc592cf908f410358491cf9344f76d105
Your Name <you@example.com>
2021-03-24 21:17:50 +0100

    Rozbudowa flaska - komentarze

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+from flask import redirect, request, Flask, render_template
+from statistics import mean
 
 app = Flask(__name__)
+
+comments_list = [{
+    'nick': 'Piotrek',
+    'rating': 3,
+    'comment': 'costam'
+}, {
+    'nick': 'Mirek',
+    'rating': 4,
+    'comment': 'costam'
+}]
 
 
 @app.route('/')
@@ -13,6 +24,17 @@ def products():
     return render_template('produkty.html')
 
 
-@app.route('/bye/<int:siemanko>')
-def bye(siemanko):
-    return f'Bye, World {siemanko}'
+@app.route('/comments')
+def comments():
+    avg = mean([c['rating'] for c in comments_list])
+    return render_template('komentarze.html', cl=comments_list, rating_avg=avg)
+
+
+@app.route('/add_comment', methods=['POST'])
+def add_comment():
+    comments_list.append({
+        'nick': request.form.get('nick'),
+        'rating': int(request.form.get('rating')),
+        'comment': request.form.get('comment')
+    })
+    return redirect('/comments')
